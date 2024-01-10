8bd0cf32e0bb93342e72fe245d44a86ca813df21
right101 <raya.mogilev@gmail.com>
2024-01-10 21:23:34 +0000

    files added

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+    backend "s3" {
+        bucket = "new_project-"
+        region = "us-east-1"
+        key = "new_project/virginia/terraform.tfstate"
+        dynamodb_table = "new_project-virginia-state-lock"
+    }
+}
