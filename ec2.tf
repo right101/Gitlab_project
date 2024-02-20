c7dfebb52301d533be04696b15d2a265ac5c0919
right101 <raya.mogilev@gmail.com>
2024-02-20 22:22:08 +0000

    instance subnet id updated

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "gitlab" {
     ami                    = data.aws_ami.ubuntu.id 
     instance_type          = var.instance_type
-    subnet_id              = aws_subnet.public_subnet.id
+    subnet_id              = aws_subnet.public_subnet[0].id
     vpc_security_group_ids = [aws_security_group.project_sg.id]
     key_name               = aws_key_pair.bastion_key.key_name
     user_data              = file("gitlab.sh")
