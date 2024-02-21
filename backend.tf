2a185839b3e3008aa5e63890d1c6f89f58d0abfa
right101 <raya.mogilev@gmail.com>
2024-02-21 04:44:19 +0000

    backend modified

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
         bucket = "terraform-backend-raya"
         region = "us-east-1"
         key = "new_project/terraform.tfstate"
-        dynamodb_table = "terraform-session-sep-state-lock"
+        #dynamodb_table = "terraform-session-sep-state-lock"
     }
 } 
 # The region field in the Terraform S3 backend configuration specifies the AWS region where the S3 bucket for storing the Terraform state is located, not the regions where your resources will be deployed. This is a common point of confusion.
